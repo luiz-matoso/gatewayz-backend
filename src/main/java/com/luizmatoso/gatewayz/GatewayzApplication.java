9f543d1299040b69208cf5f6c44c1f9f1615e4c4
unknown <luizhmatoso1340@gmail.com>
2025-04-27 16:01:17 -0300

    feat: project setup

---
+++ src/main/java/com/luizmatoso/gatewayz/GatewayzApplication.java
@@ -0,0 +1,13 @@
+package com.luizmatoso.gatewayz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luizmatoso/gatewayz/GatewayzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizmatoso.gatewayz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
