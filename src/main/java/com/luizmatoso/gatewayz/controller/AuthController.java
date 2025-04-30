b75b8f459149b19057ddd81077d803bb0a5fc788
unknown <luizhmatoso1340@gmail.com>
2025-04-30 17:23:00 -0300

    feat: add logout api call

--- src/main/java/com/luizmatoso/gatewayz/controller/AuthController.java
+++ src/main/java/com/luizmatoso/gatewayz/controller/AuthController.java
@@ -6,6 +6,7 @@ import com.luizmatoso.gatewayz.dto.ResetPasswordRequest;
 import com.luizmatoso.gatewayz.service.AppUserDetailsService;
 import com.luizmatoso.gatewayz.service.ProfileService;
 import com.luizmatoso.gatewayz.util.JwtUtil;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
@@ -115,6 +116,20 @@ public class AuthController {
         } catch (Exception e){
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
         }
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(HttpServletResponse response){
+        ResponseCookie cookie = ResponseCookie.from("jwt", "")
+                .httpOnly(true)
+                .secure(false)
+                .path("/")
+                .maxAge(0)
+                .sameSite("Strict")
+                .build();
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body("Logged out successfully!");
     }
 
 }
